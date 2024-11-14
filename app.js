9e6db8d5c3d112d62353af38c782df91fa4eafb8
IntuzHardP <hardp@intuz.net>
2024-11-14 18:42:24 +0530

    updating the branch_level-1

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const test = "Hello world!";
 
-const test1 = "First init commit message";
-console.log(test);
+const test1 = "First init commit message - Git branch level 1";
+const test2 = "Git branch level 1";
+console.log(test, test1, test2);
